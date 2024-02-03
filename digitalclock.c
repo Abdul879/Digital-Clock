6815d1636a3020e70103c83d8fe016c601d612e9
Shaik Abdulla <132251749+Abdul879@users.noreply.github.com>
2024-02-03 18:40:17 +0530

    Add files via upload

--- digitalclock.c
+++ digitalclock.c
@@ -1,45 +1,46 @@
 #include <stdio.h>
-#include <windows.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main()
 {
-  int h, m, s;
-  int d = 1000;
-  printf("Set Time :");
-  scanf("%d%d%d",&h,&m,&s);
-  if(h>12 || m>60 || s>60)
-  {
-    printf("Error !\n");
-    exit(0);
-  }
+  int hour=0;
+  int minute=0;
+  int second=0;
+   
   while(1)
   {
-     s++;
-     if(s>59)
-     {
-       m++;
-       s=0;
-     }
-     if(m>59)
-     {
-       h++;
-       m=0;
-     }
-     if(h>12)
-     {
-       h=1;
-     }
-    
-    printf("\n Clock :");
-    printf("\n %02d:%02d:%02d",h,m,s);
-    Sleep(d);
-  //  system("cls"); 
-   } 
+      //clean the output on the screen
+	system("clear");
+      //print the time in HH:MM:SS
+	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t**************************\n");
+        printf("\t\t\t\t*********%02d:%02d:%02d*********\n",hour,minute,second);
+	printf("\t\t\t\t**************************\n");
+
+	fflush(stdout);
+	//increment seconds
+	second++;
+	//update hour,mints,seconds
+	if(second==60)
+	{
+	   minute+=1;
+	   second=0;
+	}
+	if(minute==60)
+	{
+	  hour+=1;
+	  minute=0;
+	}
+	if(hour==24)
+	{
+	  hour=0;
+	  minute=0;
+	  second=0;
+	}
+        //wait for 1 second
+	sleep(1);
+    }
+    return 0;
 }
-
-
-
-
-
-
-
+	
